8fad4d518bd6f8938de5d8407cd7a4a8b857fc6f
Mathies Van den Bulcke <mathies.van.den.bulcke@student.howest.be>
2024-11-26 11:46:07 +0100

    Add Comments To cactuslib.s

--- src/cactuslib.s
+++ src/cactuslib.s
@@ -4,8 +4,6 @@ CACTUS_STARTPOS_X = 220
 
 cactus_type: .res 1
 
-is_big: .res 1
-
 ; Cactus texture parts
 CACTUS_TOP_START = 12
 CACTUS_TOP_END   = 14
@@ -19,82 +17,81 @@ CACTUS_SMALL_END    = 16
 .segment "CODE"
 
 .proc generate_cactus
-    LDA #2
-    STA operation_address
+    LDA #2                  ; Load the dividor in A (so modulo returns 0 or 1)
+    STA operation_address   ; We will divide A by operation address
 
-    JSR prng
-    JSR divide
+    JSR prng                ; Generate a random number and store it in A
+    JSR divide              ; Divide A by operation address
 
-    STY is_big
+    TYA                     ; Move remainder into A
 
-    LDA #1
-    CMP is_big
-    BEQ big_cactus
-    JSR make_small_cactus
+    CMP #1                  ; If random number = 1 we generate a big cactus (else if = 0 we generate a small cactus)
+    BEQ big_cactus          ; Branch to label "big_cactus" if A = 1
+    JSR make_small_cactus   ; If it didn't branch A = 0 which means we should generate a small cactus
     RTS
 
     big_cactus:
-    JSR make_big_cactus
+    JSR make_big_cactus     ; A = 1 which means we should generate a big cactus which this subroutine does
     RTS
     
 .endproc
 
 .proc make_big_cactus
 
-    LDA #CACTUS_STARTPOS_X
-    STA oam_px
+    LDA #CACTUS_STARTPOS_X                      ; Load the starting x-pos into A 
+    STA oam_px                                  ; Tell "draw_sprite" at what x-pos it should start drawing the sprite
 
-    LDA #FLOOR_HEIGHT
-    STA oam_py
+    LDA #FLOOR_HEIGHT                           ; Load the starting y-pos into A
+    STA oam_py                                  ; Tell "draw_sprite" what height to start drawing the sprite
 
-    LDA #(CACTUS_BOT_END - CACTUS_BOT_START)
-    STA operation_address
+    LDA #(CACTUS_BOT_END - CACTUS_BOT_START)    ; Give the range of possible cactus bottom parts
+    STA operation_address                       ; We will divide this range by operation_address
 
-    JSR prng
-    JSR divide
+    JSR prng                                    ; Generate a random number
+    JSR divide                                  ; Divide A by operation_address
 
-    TYA
-    CLC
-    ADC #CACTUS_BOT_START
+    TYA                                         ; Move the remainder to A to add with carry 
+    CLC                                         ; Make sure carry is clear
+    ADC #CACTUS_BOT_START                       ; Add the start of the range of bottom halves to random number to get a random index of cactus bottom
 
-    JSR draw_sprite
+    JSR draw_sprite                             ; Draw the bottom half of the big cactus
 
-    LDA #(FLOOR_HEIGHT - 8)
-    STA oam_py
+    LDA #(FLOOR_HEIGHT - 8)                     ; Draw top half of the big cactus 8 pixels above the bottom half
+    STA oam_py                                  ; Tell "draw_sprite" what height to start drawing the sprite (x-pos stays the same)
 
-    LDA #(CACTUS_TOP_END - CACTUS_TOP_START)
-    STA operation_address
+    LDA #(CACTUS_TOP_END - CACTUS_TOP_START)    ; Give the range of possible cactus top parts
+    STA operation_address                       ; We will divide this range by operation_address
 
-    JSR prng
-    JSR divide
+    JSR prng                                    ; Generate a random number
+    JSR divide                                  ; Divide A by operation_address
 
-    TYA
-    CLC
-    ADC #CACTUS_TOP_START
+    TYA                                         ; Move the remainder to A to add with carry
+    CLC                                         ; Make sure carry is clear
+    ADC #CACTUS_TOP_START                       ; Add the start of the range of top halves to random number to get random index of cactus top halves
 
-    JSR draw_sprite
+    JSR draw_sprite                             ; Draw top half of the big cactus
 
     RTS
 .endproc
 
 .proc make_small_cactus
-    LDA #CACTUS_STARTPOS_X
-    STA oam_px
+    LDA #CACTUS_STARTPOS_X                          ; Load the starting x-pos into A 
+    STA oam_px                                      ; Tell "draw_sprite" what x-pos to start drawing the sprite
 
-    LDA #FLOOR_HEIGHT
-    STA oam_py
+    LDA #FLOOR_HEIGHT                               ; Load the starting y-pos into A
+    STA oam_py                                      ; Tell "draw_sprite" what height to start drawing the sprite
 
-    LDA #(CACTUS_SMALL_END - CACTUS_SMALL_START)
-    STA operation_address
+    LDA #(CACTUS_SMALL_END - CACTUS_SMALL_START)    ; Give the range of possible small cacti
+    STA operation_address                           ; We will divide this range by operation_address
 
-    JSR prng
-    JSR divide
+    JSR prng                                        ; Generate a random number
+    JSR divide                                      ; Divide A by operation_address
 
-    TYA
-    CLC
-    ADC #CACTUS_SMALL_START
+    TYA                                             ; Tranfer the remainder to add with carry
+    CLC                                             ; Make sure carry is clear
+    ADC #CACTUS_SMALL_START                         ; Add the start of the range of small cacti to random number to get random index of small cacti
 
-    JSR draw_sprite
+    JSR draw_sprite                                 ; Draw small cactus
 
     RTS
 .endproc
