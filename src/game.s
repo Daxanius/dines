bfef3928162577f05a4513a290965d52aac4231e
BalderHuybreghs <balder.huybreghs@gmail.com>
2024-12-12 18:11:39 +0100

    Change text position

--- src/game.s
+++ src/game.s
@@ -367,7 +367,7 @@ skip_scroll:
     m_assign_16i operation_address, title_text             ; Write our title to text address
     JSR write_text                                         ; Writes the text in operation_address to the nametable
 
-    m_vram_set_address (NAME_TABLE_0_ADDRESS + 20 * 32 + 6) ; Set the address to the start nametable plus the position where we want to draw our text
+    m_vram_set_address (NAME_TABLE_0_ADDRESS + 17 * 32 + 6) ; Set the address to the start nametable plus the position where we want to draw our text
     m_assign_16i operation_address, press_play_text         ; Write our ppress play text to text address
     JSR write_text                                          ; Writes the text in operation_address to the nametable
 
